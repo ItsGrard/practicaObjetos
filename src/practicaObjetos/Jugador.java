1fc5e351eca7ccadc1ba080aefd745c51578ab5d
Katsu <katsurana@outlook.es>
2021-01-27 12:06:52 +0100

    .

--- src/practicaObjetos/Jugador.java
+++ src/practicaObjetos/Jugador.java
@@ -5,35 +5,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Jugador extends Persona{
+public class Jugador{
 	
-	private int dorsal;
-	private String posicion;
-	
-	public Jugador (String archivo_entrada) throws IOException {
-		
-		BufferedReader in = null;
-		try {
-			in = new BufferedReader (new FileReader(archivo_entrada));
-			this.nombre = in.readLine();
-			this.apellidos = in.readLine();
-			this.DNI = in.readLine();
-			this.sueldo = Integer.parseInt(in.readLine());
-			this.edad = Integer.parseInt(in.readLine());
-			this.dorsal = Integer.parseInt(in.readLine());
-			this.posicion = in.readLine();
-		} finally {
-			if (in != null) in.close();
-		}
-	}
+	// variables
 	
 	public Jugador() {
-		
+		// constructor
 	}
 	
-	public void imprimir() {
-		super.imprimir();
-		System.out.println("Dorsal: "+this.dorsal + " Posicion: "+this.posicion);	
-	}
+	// metodo imprimir
 		
 }
